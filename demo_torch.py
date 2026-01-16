a6b5364adc5789dcf0b5fb74f129fff68d107fb4
wangmu <wangmu8889@gmail.com>
2026-01-16 20:27:24 +0800

    merge

--- demo_torch.py
+++ demo_torch.py
@@ -1,5 +1,16 @@
 import torch
 
-print(torch.__version__)
-print(torch.cuda.is_available())
-print(torch.backends.mps.is_available())
+# print(torch.__version__)
+# print(torch.cuda.is_available())
+# print(torch.backends.mps.is_available())
+print(torch.tensor(1))
+print(torch.tensor([1, 2, 3], dtype=torch.float32))
+print(torch.tensor([[1, 2, 3], [4, 5, 6]]))
+print(torch.tensor([[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], [[13, 14, 15, 16], [17, 18, 19, 20], [21, 22, 23, 24]]]))
+
+float_vec = torch.tensor([11.0, 12.2, 13.0])
+print(float_vec)
+print(float_vec.dtype)
+float_vec = float_vec.to(torch.int32)
+print(float_vec.dtype)
+print(float_vec)
